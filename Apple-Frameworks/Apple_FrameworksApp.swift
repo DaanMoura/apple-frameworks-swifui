4c435af516eedf6fdcc7c435cfcd884070bf9541
Daniel Moura <daniel.lucio.moura@gmail.com>
2024-02-04 13:48:28 -0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Daniel Moura on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
